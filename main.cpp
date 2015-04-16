ea85bf0298dbea6ce8866cbb2243327d6c71d23e
Jochen Keil <jochen.keil@gmail.com>
2015-04-16 12:02:40 +0200

    Load url from command line

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <QApplication>
 #include <QWebView>
 
-int main(int argc, char *argv[])
+int main(int argc, char ** argv)
 {
   QApplication app(argc, argv);
   QWebView view;
   view.show();
-  view.load(QUrl("http://google.com"));
+  view.load(QUrl(argv[1]));
 
   return app.exec();
 }

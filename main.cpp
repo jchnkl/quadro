ab34f6fdbfa88443e26f5ecf54e4d060f86790b6
Jochen Keil <jochen.keil@gmail.com>
2015-04-17 14:48:53 +0200

    Show/Hide url bar on Enter/Leave event

--- main.cpp
+++ main.cpp
@@ -5,14 +5,12 @@
 #include <QGroupBox>
 #include <QGridLayout>
 #include <QtWidgets>
-#include <QRect>
 #include <QWebView>
 
 class Browser : public QGroupBox
 {
   public:
     Browser(void)
-      : m_UrlBarAnimation(&m_UrlBar, "visible")
     {
       // create & set transparent palette for browser window
       QPalette palette = m_View.palette();
@@ -28,9 +26,7 @@ class Browser : public QGroupBox
       this->setWindowFlags(Qt::FramelessWindowHint);
       this->setAttribute(Qt::WA_TranslucentBackground);
       this->setLayout(&m_Layout);
-      // this->grabMouse();
 
-      // connect(&m_Layout, &QWidget::mouseMoveEvent, this, &Browser::showUrlBar);
       connect(&m_View, &QWebView::loadFinished, this, &Browser::renderToImage);
       connect(&m_UrlBar, &QLineEdit::returnPressed, this, &Browser::loadUrlFromBar);
     }
@@ -59,64 +55,21 @@ class Browser : public QGroupBox
     }
 
     void
-    enterEvent(QEvent * event)
+    enterEvent(QEvent *)
     {
-      // std::cerr << __PRETTY_FUNCTION__ << std::endl;
-
-      m_UrlBar.setVisible(false);
-
-      // m_UrlBar.show();
-      m_UrlBarAnimation.setDuration(500);
-      m_UrlBarAnimation.setDirection(QAbstractAnimation::Forward);
-      // QRect startRect(Rect_Relative_to_Opener_Button);
-      // QRect endRect(Shifted_Rect_Relative_to_Opener_Button);
-      // animation.setStartValue(startRect);
-      // animation.setEndValue(endRect);
-      // animation.setStartValue(QRect(0, 0, 0, 0));
-      // animation.setEndValue(QRect(0, 0, m_UrlBar.width(), m_UrlBar.height()));
-      // m_UrlBarAnimation.setStartValue(QSize(m_UrlBar.width(), 0));
-      // m_UrlBarAnimation.setEndValue(QSize(m_UrlBar.width(), m_UrlBar.height()));
-      // m_UrlBarAnimation.setStartValue(0);
-      // m_UrlBarAnimation.setEndValue(m_UrlBar.height());
-      // m_UrlBarAnimation.start();
-
-      m_UrlBarAnimation.setStartValue(false);
-      m_UrlBarAnimation.setEndValue(true);
+      m_UrlBar.show();
     }
 
     void
-    leaveEvent(QEvent * event)
+    leaveEvent(QEvent *)
     {
-      // std::cerr << __PRETTY_FUNCTION__ << std::endl;
-
-      m_UrlBar.setVisible(true);
-      m_UrlBarAnimation.setDirection(QAbstractAnimation::Backward);
-
-      m_UrlBarAnimation.setStartValue(true);
-      m_UrlBarAnimation.setEndValue(false);
-
-      // m_UrlBarAnimation.setStartValue(QSize(m_UrlBar.width(), m_UrlBar.height()));
-      // m_UrlBarAnimation.setEndValue(QSize(m_UrlBar.width(), 0));
-
-      m_UrlBarAnimation.start();
-      // m_UrlBar.hide();
+      m_UrlBar.hide();
     }
-
-    // void
-    // mouseMoveEvent(QMouseEvent * event)
-    // {
-    //   std::cerr << "x: " << event->pos().x()
-    //             << "; y: " << event->pos().y()
-    //             << "; rx: " << event->pos().rx()
-    //             << "; ry: " << event->pos().ry()
-    //             << std::endl;
-    // }
 
   private:
     QGridLayout m_Layout;
-    QWebView m_View;
     QLineEdit m_UrlBar;
-    QPropertyAnimation m_UrlBarAnimation;
+    QWebView m_View;
 }; // Browser
 
 int main(int argc, char ** argv)

8a3b4a50a877a66b6bf20904304e11d4e84b2906
Jochen Keil <jochen.keil@gmail.com>
2015-04-22 13:02:42 +0200

    Move Browser to separate compilation module

---
+++ Browser.hpp
@@ -0,0 +1,125 @@
+#ifndef _QUADRO_BROWSER_HPP
+#define _QUADRO_BROWSER_HPP
+
+#include <iostream>
+
+#include <QApplication>
+#include <QDesktopWidget>
+#include <QGridLayout>
+#include <QGroupBox>
+#include <QLineEdit>
+#include <QWebView>
+#include <QX11Info>
+
+#include "Config.hpp"
+#include "NetWmWindowType.hpp"
+
+
+class Browser
+  : public QGroupBox
+{
+  public:
+    Browser(const Config & config)
+    {
+      // set object name for style sheet
+      this->setObjectName("Browser");
+
+      // remove the border
+      this->setStyleSheet("QGroupBox#Browser { border: 0px; }");
+
+      // hint for window manager to leave away frames/window borders
+      this->setWindowFlags(Qt::FramelessWindowHint);
+
+      // make window background transparent
+      this->setAttribute(Qt::WA_TranslucentBackground, true);
+
+      Ewmh ewmh(QX11Info::connection());
+      NetWmWindowType windowType(ewmh, this->winId());
+
+      windowType.clear(NetWmWindowType::Same).add(config.windowTypeHint());
+
+      // remove layout margins / padding
+      m_Layout.setMargin(0);
+
+      // add widgets to grid layout
+      m_Layout.addWidget(&m_UrlBar, 0, 0);
+      m_Layout.addWidget(&m_View, 1, 0);
+
+      // use grid layout
+      this->setLayout(&m_Layout);
+
+      // create & set transparent palette for browser window
+      QPalette palette = m_View.palette();
+      palette.setBrush(QPalette::Base, Qt::transparent);
+      m_View.page()->setPalette(palette);
+
+      // enable transparency for underlying window
+      m_View.setAttribute(Qt::WA_TranslucentBackground, true);
+
+      connect(&m_View, &QWebView::urlChanged, this, &Browser::onUrlChanged);
+      connect(&m_View, &QWebView::loadFinished, this, &Browser::onLoadFinished);
+      connect(&m_UrlBar, &QLineEdit::returnPressed, this, &Browser::onReturnPressed);
+
+      this->setGeometry(config.x(), config.y(), config.width(), config.height());
+
+      if (config.hasUrl()) {
+        this->loadUrl(QUrl::fromUserInput(config.url()));
+      }
+
+      if (config.fullscreen()) {
+        QRect desktop_rect = QApplication::desktop()->screenGeometry();
+        this->setGeometry(desktop_rect);
+      }
+
+      this->show();
+    }
+
+    void loadUrl(const QUrl & url)
+    {
+      m_View.load(url);
+    }
+
+  protected:
+    void
+    onUrlChanged(const QUrl & url)
+    {
+      m_UrlBar.setText(url.toString());
+    }
+
+    void
+    onLoadFinished(bool ok)
+    {
+      if (ok) {
+        QPixmap pixmap(m_View.size());
+        m_View.render(&pixmap, QPoint(), m_View.rect());
+        pixmap.save("foo.jpg");
+      }
+      std::cerr << "done" << std::endl;
+    }
+
+    void onReturnPressed(void)
+    {
+      loadUrl(QUrl::fromUserInput(m_UrlBar.text()));
+      m_UrlBar.selectAll();
+    }
+
+    void
+    enterEvent(QEvent *)
+    {
+      m_UrlBar.show();
+    }
+
+    void
+    leaveEvent(QEvent *)
+    {
+      m_UrlBar.hide();
+    }
+
+  private:
+    // Config m_Config;
+    QGridLayout m_Layout;
+    QLineEdit m_UrlBar;
+    QWebView m_View;
+}; // Browser
+
+#endif // _QUADRO_BROWSER_HPP
--- quadro.pro
+++ quadro.pro
@@ -8,4 +8,4 @@ greaterThan(QT_MAFOR_VERSION, 4): QT += widets
 TARGET = quadro
 TEMPLATE = app
 
-SOURCES += Main.cpp Config.hpp Ewmh.hpp NetWmWindowType.hpp
+SOURCES += Main.cpp Browser.hpp Config.hpp Ewmh.hpp NetWmWindowType.hpp
